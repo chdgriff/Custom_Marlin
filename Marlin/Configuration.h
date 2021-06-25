a0ba4668289285d6443a9550aa9242677ebfc871
Christopher Griffis <chrisgriffis@gmail.com>
2021-06-25 00:48:41 -0700

    raised juntion deviation, lowered e default jerk

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -961,7 +961,7 @@
   #endif
 #endif
 
-#define DEFAULT_EJERK    5.0  // May be used by Linear Advance
+#define DEFAULT_EJERK    3.75  // May be used by Linear Advance
 
 /**
  * Junction Deviation Factor
@@ -971,7 +971,7 @@
  *   https://blog.kyneticcnc.com/2018/10/computing-junction-deviation-for-marlin.html
  */
 #if DISABLED(CLASSIC_JERK)
-  #define JUNCTION_DEVIATION_MM 0.055 // (mm) Distance from real junction edge
+  #define JUNCTION_DEVIATION_MM 0.07 // (mm) Distance from real junction edge
   #define JD_HANDLE_SMALL_SEGMENTS    // Use curvature estimation instead of just the junction angle
                                       // for small segments (< 1mm) with large junction angles (> 135°).
 #endif
